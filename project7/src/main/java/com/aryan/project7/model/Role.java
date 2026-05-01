6ac4967a719eb26d28f00b676b8af04517bc13cb
Aryan raj <aryanbanthu@gmail.com>
2026-05-01 15:13:58 +0530

    Created Role class

--- project7/src/main/java/com/aryan/project7/model/Role.java
+++ project7/src/main/java/com/aryan/project7/model/Role.java
@@ -1,6 +1,20 @@
 package com.aryan.project7.model;
 
-public enum Role {
-    USER,
-    ADMIN
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+@Entity
+@Table(name = "user_role")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "role_id")
+    private UUID id = UUID.randomUUID();
+    @Column(unique = true,nullable = false)
+    private String name;
 }

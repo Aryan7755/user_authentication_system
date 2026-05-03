a0a355c8152b81d70aec38e28c9b42b60dc780a4
Aryan raj <aryanbanthu@gmail.com>
2026-05-03 22:42:49 +0530

    Added UserDto

---
+++ project7/src/main/java/com/aryan/project7/dtos/UserDto.java
@@ -0,0 +1,32 @@
+package com.aryan.project7.dtos;
+
+import com.aryan.project7.model.Provider;
+import com.aryan.project7.model.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+    private String image;
+    private boolean enabled = true;
+    private Instant createdAt=Instant.now();
+    private Instant updatedAt=Instant.now();
+    @Enumerated(EnumType.STRING)
+    private Provider provider=Provider.LOCAL;
+    private Set<RoleDto> roles = new HashSet<>();
+}

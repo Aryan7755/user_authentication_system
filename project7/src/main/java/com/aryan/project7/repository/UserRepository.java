47edd8c30893704a8bd38caa1d301f9197e105ea
Aryan raj <aryanbanthu@gmail.com>
2026-05-03 22:42:38 +0530

    Added UserRepository

--- project7/src/main/java/com/aryan/project7/repository/UserRepository.java
+++ project7/src/main/java/com/aryan/project7/repository/UserRepository.java
@@ -4,23 +4,11 @@ import com.aryan.project7.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 
-    /**
-     * Used during Login to find the user trying to authenticate.
-     * We use Optional to avoid NullPointerExceptions if the user isn't found.
-     */
-    Optional<User> findByUsername(String username);
-
-    /**
-     * Used during Registration to check if a username is already taken.
-     */
-    Boolean existsByUsername(String username);
-
-    /**
-     * Used during Registration to check if an email is already registered.
-     */
-    Boolean existsByEmail(String email);
 }

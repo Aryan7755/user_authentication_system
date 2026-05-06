981fa1f4f9b7ae1ae57ae585a75c2c89808f91d5
Aryan raj <aryanbanthu@gmail.com>
2026-05-06 18:18:48 +0530

    Added service for loading user by username

---
+++ project7/src/main/java/com/aryan/project7/security/CustomUserDetailService.java
@@ -0,0 +1,24 @@
+package com.aryan.project7.security;
+
+import com.aryan.project7.exception.ResourceNotFoundException;
+import com.aryan.project7.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userRepository
+                .findByEmail(username)
+                .orElseThrow(()-> new ResourceNotFoundException("Invalid email or password!"));
+
+    }
+}

a3bc4edb5f363e2f2120055b02ec7d9ccf9b6bfb
Aryan raj <aryanbanthu@gmail.com>
2026-05-06 18:17:19 +0530

    Added controller for register user api

---
+++ project7/src/main/java/com/aryan/project7/controller/AuthController.java
@@ -0,0 +1,21 @@
+package com.aryan.project7.controller;
+
+import com.aryan.project7.dtos.UserDto;
+import com.aryan.project7.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    public ResponseEntity<UserDto> registerUser(@RequestBody UserDto userDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerUser(userDto));
+    }
+}

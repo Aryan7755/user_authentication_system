cd2bbbff16b5a3db4fd5c45f69ed204ce9d50315
Aryan raj <aryanbanthu@gmail.com>
2026-05-13 21:47:01 +0530

    Updated RoleDto

--- project7/src/main/java/com/aryan/project7/dtos/RoleDto.java
+++ project7/src/main/java/com/aryan/project7/dtos/RoleDto.java
@@ -1,10 +1,13 @@
 package com.aryan.project7.dtos;
 
-import jakarta.persistence.Column;
-
 import java.util.UUID;
 
+// This represents a user's role (like ADMIN or USER) when sending data back and forth
 public class RoleDto {
-    private UUID id ;
+
+    // The unique ID for the role
+    private UUID id;
+
+    // The actual role name—usually something like "ROLE_USER" or "ROLE_ADMIN"
     private String name;
 }

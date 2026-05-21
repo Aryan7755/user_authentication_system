54939563da076c15630d329900b25e7deb089b0c
Aryan raj <aryanbanthu@gmail.com>
2026-05-21 14:12:23 +0530

    Added ValidationMessages class for field entries

---
+++ AuthAppBackend/src/main/java/com/aryan/project7/config/ValidationMessages.java
@@ -0,0 +1,10 @@
+package com.aryan.project7.config;
+
+public class ValidationMessages {
+    public static final String EMAIL_REQUIRED = "Email is required";
+    public static final String EMAIL_ALREADY_EXISTS = "User with this email already exists";
+    public static final String USER_NOT_FOUND = "User not found with the given ID";
+    public static final String PASSWORD_REQUIRED = "Password is required";
+
+    private ValidationMessages() {}
+}

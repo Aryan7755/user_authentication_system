2b442ad2c8f67b07c1d7b042c64eadf7fc3635b2
Aryan raj <aryanbanthu@gmail.com>
2026-05-21 12:17:46 +0530

    Updated LoginRequest to handle empty or null values

--- AuthAppBackend/src/main/java/com/aryan/project7/dtos/LoginRequest.java
+++ AuthAppBackend/src/main/java/com/aryan/project7/dtos/LoginRequest.java
@@ -2,7 +2,10 @@ package com.aryan.project7.dtos;
 
 // This is the simple data packet we expect when someone tries to log in
 public record LoginRequest(
-        String email,     // The user's account email
-        String password   // Their plain-text password (which we'll hash and check later)
-) {
-}
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email format is invalid")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        String password
+) {}

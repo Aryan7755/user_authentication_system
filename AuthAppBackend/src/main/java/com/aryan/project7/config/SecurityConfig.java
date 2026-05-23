a6e22ba4f1673cfa2d63a1c67f92bf0ef1db6d4b
Aryan raj <aryanbanthu@gmail.com>
2026-05-23 22:10:56 +0530

    Updated SecurityConfig

--- AuthAppBackend/src/main/java/com/aryan/project7/config/SecurityConfig.java
+++ AuthAppBackend/src/main/java/com/aryan/project7/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.aryan.project7.config;
 
 import com.aryan.project7.dtos.ApiError;
+import com.aryan.project7.security.CustomUserDetailService;
 import com.aryan.project7.security.JwtAuthenticationFilter;
 import com.aryan.project7.security.OAuth2FailureHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,8 +9,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,7 +28,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-
+@EnableMethodSecurity
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -39,6 +43,24 @@ public class SecurityConfig {
         this.authenticationSuccessHandler = authenticationSuccessHandler;
         this.oauth2FailureHandler = oauth2FailureHandler;
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(
+            CustomUserDetailService userDetailsService,
+            PasswordEncoder passwordEncoder
+    ) {
+
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+
+        return provider;
+    }
+
+
+
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {

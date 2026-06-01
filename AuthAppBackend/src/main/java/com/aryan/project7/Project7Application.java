97232c7f9387852ae08986d4a5b33517b23e39d3
Aryan raj <aryanbanthu@gmail.com>
2026-06-01 23:01:23 +0530

    feat: enable scheduling for background cleanup tasks

--- AuthAppBackend/src/main/java/com/aryan/project7/Project7Application.java
+++ AuthAppBackend/src/main/java/com/aryan/project7/Project7Application.java
@@ -2,11 +2,13 @@ package com.aryan.project7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 // The heart of the application.
 // @SpringBootApplication tells Spring to look for configurations,
 // enable autoconfiguration, and scan for components in this package.
 @SpringBootApplication
+@EnableScheduling // FIX: This enables the @Scheduled tasks (like token cleanup)
 public class Project7Application {
 
 	public static void main(String[] args) {

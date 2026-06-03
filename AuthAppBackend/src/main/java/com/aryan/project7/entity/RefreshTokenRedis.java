fd47baf1b21b706399865b2a7e61841c78320c1e
Aryan raj <aryanbanthu@gmail.com>
2026-06-03 15:21:50 +0530

    feat(cache): add TimeToLive support to Redis token entity for automatic expiration

--- AuthAppBackend/src/main/java/com/aryan/project7/entity/RefreshTokenRedis.java
+++ AuthAppBackend/src/main/java/com/aryan/project7/entity/RefreshTokenRedis.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 
 import java.io.Serializable;
 
@@ -15,4 +16,7 @@ public class RefreshTokenRedis implements Serializable {
     private String tokenHash; // SHA-256 hash of the token
     private String userId;
     private long expiryDate;
+
+    @TimeToLive // Spring Data Redis uses this to automatically expire the key
+    private Long ttl;
 }
